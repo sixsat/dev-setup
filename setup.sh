c5eec5a22e4646f44c642ee7899d43436b290514
sat <77387934+sixsat@users.noreply.github.com>
2026-01-12 23:28:21 +0700

    add setup script

---
+++ setup.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+if ! command -v brew &> /dev/null; then
+    echo "🍺 Installing Homebrew..."
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+    # Add Homebrew to PATH
+    echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> "$HOME/.zprofile"
+    eval "$(/opt/homebrew/bin/brew shellenv)"
+fi
+
+brew update
+brew install git
+
+GIT_REPO="https://github.com/sixsat/dev-setup.git"
+TARGET_DIR="$HOME/Documents/personal/dev-setup"
+if [ ! -d "$TARGET_DIR" ]; then
+    echo "📥 Cloning repo..."
+    git clone "$GIT_REPO" "$TARGET_DIR"
+else
+    echo "📂 Repo already exist. Updating..."
+    git -C "$TARGET_DIR" pull
+fi
+
+echo "📦 Installing packages from Brewfile..."
+if [ -f "$TARGET_DIR/Brewfile" ]; then
+    brew bundle --file="$TARGET_DIR/Brewfile"
+fi
+
+echo "🔡 Installing MesloLGS NF fonts..."
+FONT_DIR="$HOME/Library/Fonts"
+FONT_BASE_URL="https://github.com/romkatv/powerlevel10k-media/raw/master"
+fonts=("MesloLGS%20NF%20Regular.ttf" "MesloLGS%20NF%20Bold.ttf" "MesloLGS%20NF%20Italic.ttf" "MesloLGS%20NF%20Bold%20Italic.ttf")
+for font in "${fonts[@]}"; do
+    font_filename="${font//%20/ }" # Replaces %20 with space
+    if [ ! -f "$FONT_DIR/$font_filename" ]; then
+        curl -L "$FONT_BASE_URL/$font" -o "$FONT_DIR/$font_filename"
+    fi
+done
+
+if [ ! -d "$HOME/.oh-my-zsh" ]; then
+    echo "🪄 Installing Oh My Zsh..."
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+fi
+
+P10K_DIR="${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
+if [ ! -d "$P10K_DIR" ]; then
+    echo "🎨 Installing Powerlevel10k..."
+    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "$P10K_DIR"
+fi
+
+echo "⚙️ Copying configurations..."
+KARABINER_CFG_DIR="$HOME/.config/karabiner"
+VSCODE_USER_DIR="$HOME/Library/Application Support/Code/User"
+GHOSTTY_CFG_DIR="$HOME/Library/Application Support/com.mitchellh.ghostty"
+cp -f "$TARGET_DIR/dotfiles/.zshrc" "$HOME/.zshrc"
+cp -f "$TARGET_DIR/dotfiles/.p10k.zsh" "$HOME/.p10k.zsh"
+mkdir -p "$KARABINER_CFG_DIR"
+cp -f "$TARGET_DIR/dotfiles/.config/karabiner/karabiner.json" "$KARABINER_CFG_DIR/karabiner.json"
+mkdir -p "$VSCODE_USER_DIR"
+cp -f "$TARGET_DIR/vscode/settings.json" "$VSCODE_USER_DIR/settings.json"
+cp -f "$TARGET_DIR/vscode/keybindings.json" "$VSCODE_USER_DIR/keybindings.json"
+mkdir -p "$GHOSTTY_CFG_DIR"
+cp -f "$TARGET_DIR/ghostty/config" "$GHOSTTY_CFG_DIR/config"
+
+echo "✅ Setup Complete!"
+echo "Run: source ~/.zshrc"
